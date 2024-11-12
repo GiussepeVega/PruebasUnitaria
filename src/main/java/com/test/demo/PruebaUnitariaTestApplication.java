21ed68aa1cd0093e298e75c23ed1d39af4fa95ed
Aaron Mayhua <113055771+AaronMayhua@users.noreply.github.com>
2024-11-11 21:27:11 -0500

    Iniciando QA

---
+++ src/main/java/com/test/demo/PruebaUnitariaTestApplication.java
@@ -0,0 +1,13 @@
+package com.test.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaUnitariaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaUnitariaTestApplication.class, args);
+	}
+
+}
