9f0668e3f9dc29568f214539cada93dfb8e0e4e7
GiussepeVega <GiussepeVega@users.noreply.github.com>
2024-11-12 14:13:07 -0500

    front listo

--- src/main/java/com/test/demo/controller/SumaController.java
+++ src/main/java/com/test/demo/controller/SumaController.java
@@ -2,16 +2,25 @@ package com.test.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SumaController {
+	
+	@GetMapping("/calculador")
+	public String mostrarForm() {
+	    return "index";
+	}
+	
 	@PostMapping("/resultado")
-	public String sumar(@RequestParam("num1") double num1, @RequestParam("num2") double num2, Model model) {
-	    double resultado = num1 + num2;
+	public String sumar(@RequestParam("num1") int num1, @RequestParam("num2") int num2, Model model) {
+	    int resultado = num1 + num2;
 	    System.out.println("Resultado calculado: " + resultado);
 	    model.addAttribute("resultado", resultado);
+	    model.addAttribute("numero1", num1);
+	    model.addAttribute("numero2", num2);
 	    return "resultado";
 	}
 }
